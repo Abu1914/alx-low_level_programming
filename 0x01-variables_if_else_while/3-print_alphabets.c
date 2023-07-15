21bc365d43008a434c3857dc6c8f58a838e0a80a
Abu1914 <dmuktaru@gmail.com>
2023-07-15 03:27:55 -0700

    fourth task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - lowercase alphabets
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	while (LETTER <= 'Z')
+	{
+	putchar(LETTER);
+	LETTER++;
+	}
+	putchar('\n');
+	return (0);
+}
