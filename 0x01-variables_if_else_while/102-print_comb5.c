5a9fa75a1dffe6d2f9b7a7d89b36ded68ebd34c7
Abu1914 <dmuktaru@gmail.com>
2023-07-15 06:12:12 -0700

    Thirteen task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,26 +6,27 @@
 * Return: Always 0 (Success)
 */
 
-int main(void)
+void printTwoDigitNumbers(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (num1 = 10; num1 <= 99; num1++)
+	{
+	for (num2 = 10; num2 <= 99; num2++)
 	{
 	putchar((num1 / 10) + '0');
 	putchar((num1 % 10) + '0');
 	putchar(' ');
 	putchar((num2 / 10) + '0');
 	putchar((num2 % 10) + '0');
-
-	if (num1 == 98 && num2 == 99)
-	continue;
-	putchar(',');
-	putchar(' ');
 	putchar('\n');
 	}
 	}
 	}
-	printTwoDigitNumbers();
+	int main(void)
+	{
+
+	printTwoDigitNumber();
+
 	return (0);
 }
