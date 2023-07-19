4f363b8fab0c90e815b3678344392386fde740ec
Abu1914 <dmuktaru@gmail.com>
2023-07-18 18:14:44 -0700

    1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,5 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to98(int n);
-void print_times_table(int n);  
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
