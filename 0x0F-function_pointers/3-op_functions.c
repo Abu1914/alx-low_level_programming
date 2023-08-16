1e649b5e90b989ca7feab59124e09e1ebdd2b815
Abu1914 <dmuktaru@gmail.com>
2023-08-16 14:32:32 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,30 +1,66 @@
 #include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
+/**
+ * op_add - a function used to add 2 numbers
+ * @a: First number to add
+ * @b: Second number
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
 /**
- * get_op_func - a function that selects the correct
- * function to perform the operation
- * @s: the operand to look out for
- * Return: the function used
+ * op_sub - a function used to subtract 2 numbers
+ * @a: First number
+ * @b: Second number
+ * Return: the subtraction
  */
-int (*get_op_func(char *s))(int, int)
+int op_sub(int a, int b)
 {
-		op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	return (a - b);
+}
 
-	i = 0;
-	while (i < 5)
+/**
+ * op_mul - a function used to multiply 2 numbers
+ * @a: First number
+ * @b: Second number
+ * Return: the multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a function used to divide 2 numbers
+ * @a: First number
+ * @b: Second number
+ * Return: the division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (ops[i].op[0] == s[0])
-			return (ops[i].f);
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (NULL);
+	return (a / b);
+}
+/**
+ * op_mod - a function used to find the modulo of 2 numbers
+ * @a: First number
+ * @b: Second number
+ * Return: the modulo
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
