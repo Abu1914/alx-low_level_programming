1a4a5f682374de912eaaa83fda622eb00c169b8e
Abu1914 <dmuktaru@gmail.com>
2024-04-13 13:00:16 +0000

    search algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,7 @@ int binary_search(int *array, size_t size, int value);
 void print_bin(int *array, size_t low, size_t high);
 int exponential_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value)
+skiplist_t *linear_skip(skiplist_t *list, int value)
+
 
 #endif
