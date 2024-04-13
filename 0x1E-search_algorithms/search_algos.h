489f99b6b4b77927a28124f6e7f311c8f668fd85
Abu1914 <dmuktaru@gmail.com>
2024-04-13 11:48:15 +0000

    103-exponential.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,6 +11,7 @@ int interpolation_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_bin(int *array, size_t low, size_t high);
+int exponential_search(int *array, size_t size, int value);
 
 
 #endif
